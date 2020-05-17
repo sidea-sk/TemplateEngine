ba6e4e4060c3feef435af2cb73fc98896d67e674
mics <michal.strbka@gmail.com>
2020-05-17 17:52:40 +0200

    116: Processing base

---
+++ Source/Docx.Tests/TestBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Docx.Tests
+{
+    public abstract class TestBase
+    {
+        private readonly string _samplesFolder;
+        private readonly string _outputFolder;
+
+        protected TestBase(string samplesSubFolder)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            _samplesFolder = $"../../../../Samples/{samplesSubFolder}";
+            _outputFolder = $"../../../../TestOutputs/{samplesSubFolder}";
+        }
+
+        protected void Process(string docxSampleFileName)
+            => this.Process(docxSampleFileName, EngineConfig.Default);
+
+        protected void Process(string docxSampleFileName, EngineConfig config)
+        {
+            if (!Directory.Exists(_outputFolder))
+            {
+                Directory.CreateDirectory(_outputFolder);
+            }
+
+            var outputFileName = $"{_outputFolder}/{docxSampleFileName}.docx";
+            if (File.Exists(outputFileName))
+            {
+                File.Delete(outputFileName);
+            }
+
+            var inputFileName = $"{_samplesFolder}/{docxSampleFileName}.docx";
+            using var templateStream = File.Open(inputFileName, FileMode.Open, FileAccess.Read);
+
+            var engine = new DocumentEngine(config);
+            var docx = engine.Run(templateStream);
+
+            File.WriteAllBytes(outputFileName, docx);
+        }
+    }
+}
---
+++ Source/Docx/StreamExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace Docx
+{
+    public static class StreamExtensions
+    {
+        public static MemoryStream AsMemoryStream(this byte[] data)
+        {
+            return new MemoryStream(data);
+        }
+
+        public static Stream AsStream(this byte[] data)
+            => data.AsMemoryStream();
+    }
+}
