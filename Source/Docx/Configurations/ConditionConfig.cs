4e91e5f1cfffa39aa78a46cf1bdd8ce5c1851f85
mics <michal.strbka@gmail.com>
2020-05-17 17:00:33 +0200

    97 | 101: Engine base interface and configurations

---
+++ Source/Docx/Configurations/ConditionConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace Docx
+{
+    public class ConditionConfig
+    {
+        public static readonly ConditionConfig Default = new ConditionConfig("?", "?");
+
+        public ConditionConfig(string beginCondition, string endCondition)
+        {
+            this.Begin = beginCondition;
+            this.End = endCondition;
+        }
+
+        /// <summary>
+        /// Engine will search for pattern (Placeholder.Start|variableName|BeginCondition|Placeholder.End)
+        /// </summary>
+        public string Begin { get; }
+
+        /// <summary>
+        /// Engine will search for pattern (Placeholder.Start|EndCondition|variableName|Placeholder.End)
+        /// </summary>
+        public string End { get; }
+    }
+}
---
+++ Source/Docx/Configurations/EngineConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace Docx
+{
+    public class EngineConfig
+    {
+        public static readonly EngineConfig Default = new EngineConfig(PlaceholderConfig.Default, ArrayConfig.Default, ConditionConfig.Default);
+
+        public EngineConfig(
+            PlaceholderConfig placeholder,
+            ArrayConfig arrayConfig,
+            ConditionConfig condition)
+        {
+            this.Placeholder = placeholder;
+            this.Array = arrayConfig;
+            this.Condition = condition;
+        }
+
+        public PlaceholderConfig Placeholder { get; }
+        public ArrayConfig Array { get; }
+        public ConditionConfig Condition { get; }
+    }
+}
