57240c9a32e921f0fb31417a7686d40e42064fe3
mics <michal.strbka@gmail.com>
2020-06-02 14:24:38 +0200

    Static methods to create SimpleModel and ImageModel

---
+++ Source/Docx/DataModel/Factories/ImageModelFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Docx.DataModel.Factories
+{
+    public static class ImageModelFactory
+    {
+        public static ImageModel ToImageModel(this byte[] data, string name, string imageName)
+        {
+            return new ImageModel(name, imageName, data);
+        }
+    }
+}
---
+++ Source/Docx/DataModel/Factories/SimpleModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Docx.DataModel.Factories
+{
+    public static class SimpleModelFactory
+    {
+        public static SimpleModel ToSimpleModel(this int value, string name, string format = "")
+        {
+            return new SimpleModel(name, value.ToString(format));
+        }
+
+        public static SimpleModel ToSimpleModel(this string value, string name)
+        {
+            return new SimpleModel(name, value);
+        }
+
+        public static SimpleModel ToSimpleModel(this DateTime value, string name, string format = "")
+        {
+            return new SimpleModel(name, value.ToString(format));
+        }
+
+        public static SimpleModel ToSimpleModel(this decimal value, string name, string format = "")
+        {
+            return new SimpleModel(name, value.ToString(format));
+        }
+    }
+}
