7255f33fb187a025b94bf0c23d6d4a47feda6d40
Michal Strbka <michal.strbka@gmail.com>
2020-10-16 22:44:30 +0200

    json

---
+++ Source/Docx.Tests/JsonTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Docx.DataModel;
+using Xunit;
+
+namespace Docx.Tests
+{
+    public class JsonTests
+    {
+        [Fact]
+        public void Serialize()
+        {
+            var root = new ObjectModel(
+                "root",
+                new SimpleModel("simple", "1"),
+                new ConditionModel("conditionTrue", () => true),
+                new ConditionModel("conditionFalse", () => false),
+                new CollectionModel(
+                    "collection",
+                    new Model[] {
+                        new SimpleModel("$c", "a")
+                    },
+                    new Model[0]
+                )
+            );
+
+            // var json = root.SerializeToJson();
+        }
+    }
+}
